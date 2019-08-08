bb18d6c014f40bc2b5d137c2c914680facbde83a
Allan Denot <adenot@gmail.com>
2019-08-08 15:21:29 +1000

    Removing certificates from module as due to bug it always drift due to domain order not being consistent

--- _variables.tf
+++ _variables.tf
@@ -2,9 +2,3 @@ variable "route53_domain" {
   type        = "string"
   description = "Domains to create a public hosted zone"
 }
-
-variable "acm_certificate_domains" {
-  type        = "string"
-  default     = ""
-  description = "List of ACM certificates to create (comma separated string)"
-}
