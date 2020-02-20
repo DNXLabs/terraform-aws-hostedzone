cdbee99cca4317a687c96279758827bb8d121f9c
Arthur Diniz <arthurbdiniz@gmail.com>
2020-02-20 17:44:34 +1100

    Fix quoted type constraints deprecation
    Signed-off-by: Arthur Diniz <arthurbdiniz@gmail.com>

--- _outputs.tf
+++ _outputs.tf
@@ -1,7 +1,7 @@
 output "route53_zone_ns" {
-  value = "${aws_route53_zone.public.name_servers}"
+  value = aws_route53_zone.public.name_servers
 }
 
 output "route53_domain" {
-  value = "${aws_route53_zone.public.name}"
+  value = aws_route53_zone.public.name
 }
